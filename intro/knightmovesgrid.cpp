210a68efbf56b9933ca89a10d3aeeef8badc58df
horseshoeman-clou <218686597+horseshoeman-clou@users.noreply.github.com>
2026-04-17 17:23:45 +0530

    Solve Cses Knight Moves Grid

--- intro/knightmovesgrid.cpp
+++ intro/knightmovesgrid.cpp
@@ -1,45 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
+const int N=1000;
 
+int grid[N][N];
+
+int dx[8]={1,1,-1,-1,-2,2,-2,2};
+int dy[8]={-2,2,-2,2,1,1,-1,-1};
+
+void solve(){
 int n;
 cin>>n;
 
-for( int i=0;i<n;i++)
-{
-for(int j=0;j<n;j++)
-{
-int x=abs(i-0);
-int y=abs(j-0);
-
-if(x<y) swap(x,y);
-
-int ans;
-
-if(x== 0 && y==0) ans=0;
-else if (x==0 && y==1) ans=3;
-else if (x==0 && y==2) ans=2;
-else if (x==1 && y==0) ans=3;
-else if(x==1 && y==1) ans=2;
-else if(x==2 && y==0) ans=2;
-else if(x==2 && y==2) ans=4;
-
-else
-{
-int d=max((x+1)/2,(x+y+2)/3);
-
-if(d%2!=((x+y)%2)) d++;
-
-ans=d;
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
+grid[i][j]=0;
 }
-cout<<ans;
-if(j+1<n) cout<<" ";
+}
+
+queue<pair<int,int>> q;
+
+q.push({0,0});
+grid[0][0]=1;
+
+while(!q.empty()){
+auto [x,y]=q.front();
+q.pop();
+
+for(int i=0;i<8;i++){
+int nx=x+dx[i];
+int ny=y+dy[i];
+
+if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+if(grid[nx][ny]!=0)continue;
+
+grid[nx][ny]=grid[x][y]+1;
+q.push({nx,ny});
+}
+}
+
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
+cout<<grid[i][j]-1<<" ";
 }
 cout<<"\n";
 }
+}
+
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+solve();
 return 0;
 }
