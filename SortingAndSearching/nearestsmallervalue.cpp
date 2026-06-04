890f2249c88c819e5c7038d6a578f0f0dd27c89f
horseshoeman-clou <218686597+horseshoeman-clou@users.noreply.github.com>
2026-06-04 09:33:21 +0530

    Solve Nearest Smaller Value

---
+++ SortingAndSearching/nearestsmallervalue.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+
+int n;
+cin>>n;
+
+stack<pair<int,int>> st;
+
+for(int i=1;i<=n;i++){
+int x;
+cin>>x;
+
+while(!st.empty() && st.top().first>=x)
+st.pop();
+
+if(st.empty())
+cout<<0;
+else
+cout<<st.top().second;
+cout<<' ';
+
+st.push({x,i});
+}
+cout<<"\n";
+return 0;
+}
